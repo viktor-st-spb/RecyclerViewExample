589b5ddc8d506be03efe80935adcda61f0928606
viktor-st-spb <viktor.st.spb@gmail.com>
2021-10-18 13:12:46 +0300

    MainActivity functionality implemented.

--- app/src/main/java/com/viktorstiblo/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/viktorstiblo/recyclerviewexample/MainActivity.kt
@@ -2,10 +2,35 @@ package com.viktorstiblo.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val exampleList = generateDummyList(100)
+
+        rvRecyclerView.adapter = ExampleAdapter(exampleList)
+        rvRecyclerView.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    private fun generateDummyList (size:Int): List<ExampleItem>{
+        val list = ArrayList<ExampleItem>()
+
+        for (i in 0 until size){
+            val drawable = when (i % 3){
+                0 -> R.drawable.ic_android
+                1 -> R.drawable.ic_audio
+                else -> R.drawable.ic_sun
+            }
+
+            val item = ExampleItem (drawable, "item $i", "Line 2" )
+            list += item
+        }
+        return list
     }
 }
